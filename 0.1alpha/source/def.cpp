971e732ff695d80b8901808d2ca6055b0bbaf7f3
Chrisky003 <42509044+Chrisky003@users.noreply.github.com>
2018-11-03 13:13:51 +0800

    Update def.cpp

--- 0.1alpha/source/def.cpp
+++ 0.1alpha/source/def.cpp
@@ -1,12 +1,10 @@
 #include "def.h"
 
-POINT::POINT()
-{
+POINT::POINT() {
     this->x = 0;
     this->y = 0;
 }
-POINT::POINT(int x, int y)
-{
+POINT::POINT(int x, int y) {
     this->x = x;
     this->y = y;
 }
