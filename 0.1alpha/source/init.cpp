1f581cf130ddb85af454b2281ef5ff1411216ec9
Chrisky003 <42509044+Chrisky003@users.noreply.github.com>
2018-11-03 13:14:08 +0800

    Update init.cpp

--- 0.1alpha/source/init.cpp
+++ 0.1alpha/source/init.cpp
@@ -4,8 +4,7 @@
 // void* _Unwind_Resume = nullptr;
 // void* __gxx_personality_v0 = nullptr;
 
-extern "C" int entry(multiboot_t *mb)
-{
+extern "C" int entry(multiboot_t *mb) {
 	CKernel kernel(mb);
 	kernel.run();
 	return 0xab1ab1ab;
