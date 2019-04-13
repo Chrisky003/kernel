18ae2e091d6f34fcf870b4a04f1ab8f2787efac0
Chrisky003 <1161557944@qq.com>
2019-04-13 16:00:45 +0800

    idt

--- source/main.cpp
+++ source/main.cpp
@@ -18,34 +18,17 @@ int main() {
 	return kernelEntry(glb_mboot_ptr);
 }
 
-class CTest {
-public:
-	CTest() {
-		puts("CTest Constructor.\n");
-	}
-	~CTest() {
-		puts("CTest Destructor.\n");
-	}
-public:
-	void func() {
-		puts("now run in \"void CTest::func()\"\n");
-		puts("now run out of \"void CTest::func()\"\n");
-	}
-} test;
-
-CTest test2;
-
 extern "C" int kernelEntry(MULTIBOOT *pmultiboot) {
-	CTest test3;
-	test.func();
-	test2.func();
-	test3.func();
 	init_debug(pmultiboot);
 	// clear();
 	puts("Hello World!\n", color::green, color::red);
 	memManage::init_mm();
 	puts("now run in protected mode.\n");
 
+	asm("sti");
+	printk("interrupt test\n");
+	asm("int $255");
+	asm("int $80");
 	//printk("0x%x\n", pmultiboot);
 	//panic("Test!");
 	modManager.init();
