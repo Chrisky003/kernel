cfaab7c26cbb9239c20091312825d7b912593e1f
christopher <1161557944@qq.com>
2019-06-18 09:24:32 +0800

    paging

--- mm/paging.cpp
+++ mm/paging.cpp
@@ -1,4 +1,5 @@
 #include "types.h"
+#include "mm/idt.h"
 #include "mm/paging.h"
 #include "io.h"
 using namespace io::console::real;
@@ -22,8 +23,11 @@ namespace paging {
         return 1;
     }
     void init() {
-        pd[0].flag_addr_and_zero = (int)&pt[0][0] | 0x3;
-        for (int i = 0; i <= (kern_end - kern_start + 1023) / 1024; i += 4) {
+        int size((kern_end - kern_start + 1023) / 1024);
+        for (int i = 0; i < (size + 1023) / 1024; i++) {
+            pd[i].flag_addr_and_zero = (int)&pt[i][0] | 0x3;
+        }
+        for (int i = 0; i < size; i += 4) {
             pt[0][i].flag_addr_and_zero= (i << 12) | 0x3;
         }
         asm("mov %0, %%cr3"::"r"(&pd));
@@ -33,10 +37,10 @@ namespace paging {
         asm volatile ("mov %%cr0, %0" : "=r" (cr0));
         cr0 |= 0x80000000;
         asm volatile ("mov %0, %%cr0" : : "r" (cr0));
-        // 切换内核栈
-        uint32 kern_stack_top = ((uint32)::kern_stack + STACK_SIZE) & 0xFFFFFFF0;
-        asm volatile ("mov %0, %%esp\n\t"
-                "xor %%ebp, %%ebp" : : "r" (kern_stack_top));
+        idt::register_interrupt_handler(14, pageFaultHandler);
         return;
+    }
+    void pageFaultHandler(idt::pt_regs pregs) {
+        printk("interrupt 14(page fault) handled!");
     }
 }
