839f1ac0eb7bdc0fff1f82e33693f2803c23348f
ETO-Chris <1161557944@qq.com>
2020-02-03 10:23:03 +0800

    format main.cpp

--- init/main.cpp
+++ init/main.cpp
@@ -1,15 +1,15 @@
-#include "stdlib.h"
 #include "debug/debug.h"
-#include "mm/memManage.h"
 #include "mm/idt.h"
-#include "timer/timer.h"
+#include "mm/memManage.h"
 #include "mm/paging.h"
+#include "stdlib.h"
+#include "timer/timer.h"
 using namespace io::console::real_console;
 using namespace debug;
 
 extern "C" {
-	//void *__dso_handle = 0;
-	int kernelEntry(MULTIBOOT *pmultiboot);
+// void *__dso_handle = 0;
+int kernelEntry(MULTIBOOT *pmultiboot);
 }
 extern "C" MULTIBOOT *glb_mboot_ptr;
 
@@ -20,19 +20,20 @@ extern "C" int kernelEntry(MULTIBOOT *pmultiboot) {
 	memManage::init_mm();
 	puts("now run in protected mode.\n");
 
-    idt::init_idt();
+	idt::init_idt();
 	printk("interrupt test\n");
 	asm("int $255");
 	asm("int $80");
-	//printk("0x%x\n", pmultiboot);
-	//panic("Test!");
+	// printk("0x%x\n", pmultiboot);
+	// panic("Test!");
 	printk("interrupt test finished.\n");
 	putc('\n');
 	printk("sizeof paging::pd is %d\n", sizeof(paging::pd));
 	printk("sizeof paging::pt is %d\n", sizeof(paging::pt));
 	printk("kernel in memory start: 0x%08X\n", kern_start);
 	printk("kernel in memory end: 0x%08X\n", kern_end);
-	printk("kernel in memory used: %d KB\n\n", (kern_end - kern_start + 1023) / 1024);
+	printk("kernel in memory used: %d KB\n\n",
+		   (kern_end - kern_start + 1023) / 1024);
 	paging::init();
 	init_timer(1000);
 	asm("sti");
